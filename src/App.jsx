a30014304daa9211f93159cfcf01201712c18f6f
David Sánchez <davidsanchez@Davids-MacBook-Pro-2.local>
2024-04-13 21:20:36 -0600

    add selection color in pokemon images

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import ScreenPokemones from './components/ScreenPokemones';
 
 function App() {
   const [pokemones, setPokemones] = useState('');
+  const [position, setPosition] = useState(0);
+
+  const [myPokeSelection, setMyPokeSelection] = useState([]);
+
+
   const pokeUrl = 'https://pokeapi.co/api/v2/pokemon';
 
   const fetchData = async (url) => {
@@ -31,9 +36,37 @@ function App() {
       return { ...pokemon, moves: attackValue };
     });
 
-    console.log(addAttack);
+    // console.log(addAttack);
     setPokemones(addAttack);
     // console.log(pokemonWithImages);
+  };
+
+  const handleSelection = (foward) => {
+    //console.log(foward);
+    if (!foward && position <= 0) return;
+    if (foward && position >= 20) return;
+    if (!foward) {
+      setPosition(position - 1);
+    } else {
+      setPosition(position + 1);
+    }
+
+    //console.log(position);
+  };
+
+  const filterSelection = () => {
+    const mySelection = pokemones.filter((value, idx) => position === idx);
+    setMyPokeSelection(mySelection);
+    console.log(mySelection);
+
+    computerSelection();
+  };
+
+  const computerSelection = () => {
+    const computerPos = Math.floor(Math.random() * 20);
+    const computerSelection = pokemones.filter((value, idx) => computerPos === idx);
+
+    console.log(computerSelection);
   };
 
   useEffect(() => {
@@ -48,23 +81,34 @@ function App() {
         <div className="layout-game">
           <div className="container-screen">
             <div className="screen-layout">
-              {pokemones && <ScreenPokemones pokemones={pokemones} />}
+              {pokemones && (
+                <ScreenPokemones pokemones={pokemones} position={position} />
+              )}
             </div>
           </div>
           <div className="buttons-container">
             {/* buttons pad*/}
             <div className="container-pad">
-              <button className="btn-right"></button>
+              <button
+                className="btn-right"
+                onClick={() => handleSelection(false)}
+              ></button>
               <div className="container-up-down">
                 <button className="btn-up"></button>
                 <button className="btn-down"></button>
               </div>
-              <button className="btn-left"></button>
+              <button
+                className="btn-left"
+                onClick={() => handleSelection(true)}
+              ></button>
             </div>
             {/* buttons select */}
             <div className="container-select">
               <div className="container-select-btn">
-                <button className="btn-select"></button>
+                <button
+                  className="btn-select"
+                  onClick={() => filterSelection()}
+                ></button>
                 <div>select</div>
               </div>
               <div className="container-start">
